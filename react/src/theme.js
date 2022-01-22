fd87d01481e9e61ce87800e83fc4b7858ec34115
Reece Atkinson <reece@croftonmail.onmicrosoft.com>
2022-01-22 11:50:01 +0000

    Theme

--- react/src/theme.js
+++ react/src/theme.js
@@ -20,8 +20,8 @@ const theme = {
     redDark: "#bb0c0c",
     redLight: "#fddddd",
     white: "#ffffff",
-    turquoise: "#1bebb9",
-    teal: "#14997a",
+    turquoise: "#000000",
+    teal: "#000000",
   },
   fontSize: {
     small: "11px",
